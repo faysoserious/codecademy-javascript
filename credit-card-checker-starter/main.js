09cd006727361373289daad1cd067fd0b00f5ebe
Feifx <32243939+faysoserious@users.noreply.github.com>
2020-11-06 17:24:16 +0100

    finish validateCred()

--- credit-card-checker-starter/main.js
+++ credit-card-checker-starter/main.js
@@ -5,6 +5,7 @@ const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
 const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
 const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
 const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
+const valid6 = [4, 5, 3, 9, 6, 8, 9, 8, 8, 7, 7, 0, 5, 7, 9, 8]
 
 // All invalid credit card numbers
 const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
@@ -27,11 +28,51 @@ const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, inval
 // Add your functions below:
 const validateCred = (array) => {
     let validation = new Array(array.length);
-    for (let i = 0; i < array.length; i++) {
-
+    validation[(validation.length)-1] = array[(validation.length)-1];
+    for (let i = array.length-1; i >= 0; i--) {
+        if ((array.length-1-i)%2 != 0) {
+            if (array[i]*2 > 9) {
+                validation[i] = (array[i] * 2 - 9);
+            } else {
+                validation[i] = (array[i] * 2);
+            };
+        } else {
+            validation[i] = (array[i]);
+        };
+        
     };
+    //validation.push(array[(array.length - 1)]);
+    let sum = 0;
+    for (const digit of validation) {
+        sum = sum + digit;
+    };
+    console.log(sum);
+    if (sum % 10 === 0) {
+        return true;
+    } else {
+        return false;
+    };
+    
 
 }
+
+console.log('VALID:')
+console.log(valid4);
+console.log(validateCred(valid4));
+console.log(valid5);
+console.log(validateCred(valid5));
+console.log(valid3);
+console.log(validateCred(valid3));
+console.log(valid6);
+console.log(validateCred(valid6));
+console.log('invalid:')
+console.log(invalid3);
+console.log(validateCred(invalid3));
+console.log(invalid4);
+console.log(validateCred(invalid4));
+console.log(invalid5);
+console.log(validateCred(invalid5));
+
 
 
 
