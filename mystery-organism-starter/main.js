d743917811673af5f2eba2444e270e1d51ddb721
Feifx <32243939+faysoserious@users.noreply.github.com>
2020-11-07 23:01:20 +0100

    finish mutate method

--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -18,10 +18,30 @@ const pAequorFactory = (number, dnaBase) => {
   return {
     specimenNum: number,
     dna : dnaBase,
+    /**
+     * .mutate() is responsible for randomly selecting a base in the object’s dna property 
+     * and changing the current base to a different base. Then .mutate() will return 
+     * the object’s dna.For example, if the randomly selected base is the 1st base 
+     * and it is 'A', the base must be changed to 'T', 'C', or 'G'. 
+     * But it cannot be 'A' again.
+     */
+    mutate() {
+      const currentBase = this.dna;
+      const mutantStand = [];
+      for (let i = 0; i < 15; i++) {
+        let item = returnRandBase();
+        do {
+          item = returnRandBase();
+        } while(item === currentBase[i]);
+        mutantStand.push(item);
+      };
+      return mutantStand;
+    }
   };
 };
-console.log(pAequorFactory(1, mockUpStrand()));
-
+const test = pAequorFactory(1,mockUpStrand());
+console.log(test.dna);
+console.log(test.mutate());
 
 
 
