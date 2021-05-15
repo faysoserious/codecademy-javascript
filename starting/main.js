0f794d2b41e856926861deff9579e8bd7099b89f
Feifx <32243939+faysoserious@users.noreply.github.com>
2021-05-15 13:03:02 +0200

    generate field and random start

--- starting/main.js
+++ starting/main.js
@@ -7,14 +7,17 @@ const pathCharacter = '*';
 
 class Field {
     constructor(fieldArray) {
-        this._oriField = fieldArray;
-        this.userX = 0;
-        this.userY = 0;
-        this._boundaryY = fieldArray.length;
-        this._boundaryX = fieldArray[0].length;
+        this._oriField = fieldArray[0];
+
+        this._userX = fieldArray[1];
+        this._userY = fieldArray[2];
+
+        this._boundaryY = this._oriField.length;
+        this._boundaryX = this._oriField[0].length;
         this.currentField = this._oriField;
         this._Hatflag = true;
     }
+    
 
     get oriField() {
         return this._oriField;
@@ -28,22 +31,36 @@ class Field {
     get Hatflag() {
         return this._Hatflag;
     }
+    get userX() {
+        return this._userX
+    }
+    get userY() {
+        return this._userY
+    }
 
     set Hatflag(condition) {
         this._Hatflag = condition;
+    }
+    set boundaryX(x) {
+        this._boundaryX = x;
+    }
+    set boundaryY(y) {
+        this._boundaryY = y;
+    }
+    set userX(x) {
+        this._userX = x;
+    }
+    set userY(y) {
+        this._userY = y;
     }
 
 
 
     judge(x, y) {
+        
         let currentX = this.userX + x;
         let currentY = this.userY + y;
-        /*        
-        console.log(`gonna be currentX = ${currentX}`);
-        console.log(`gonna be currentY = ${currentY}`);
-        console.log(`last step userY = ${this.userY}`);
-        console.log(`last step userX = ${this.userX}`);
-        */
+        
         if (currentX >= this.boundaryX || currentX < 0 || currentY >= this.boundaryY || currentY < 0) {
             console.log('sorry you hit the boundry');
 
@@ -103,7 +120,7 @@ class Field {
                 //console.log(`you entered ${direction}`);
                 break;
             case 'l':
-                this.judge(0, -1);
+                this.judge(-1, 0);
                 //console.log(`you entered ${direction}`);
                 break;
             default:
@@ -147,30 +164,54 @@ class Field {
     static generateField(height, width, percent) {
         const array = [];
         const symbols = ['O', '░'];
+        let hatX = 0;
+        let hatY = 0;
+        let startX = 0;
+        let startY = 0;
         for (let i = 0; i < height; i++) {
             let line = [];
             for (let j = 0; j < width; j++) {
                 let checker = Math.random();
-                if ( checker < (percent)) {
+                if (checker < (percent)) {
                     line.push('O');
                 } else {
                     line.push('░');
                 }
 
-//                line.push(symbols[Math.floor(Math.random() * 100) % 2]);
+                //                line.push(symbols[Math.floor(Math.random() * 100) % 2]);
             }
             array.push(line);
         }
-        let hatX = Math.floor(Math.random() * width);
-        let hatY = Math.floor(Math.random() * height);
-        array[hatY][hatX] = '^';
-        array[0][0] = '*';
+        while (hatX == 0 && hatY == 0) {
+            hatX = Math.floor(Math.random() * width);
+            hatY = Math.floor(Math.random() * height);
 
-        return array;
+        }
+
+
+        array[hatY][hatX] = '^';
+        //        array[0][0] = '*';
+        
+        do {
+            startX = Math.floor(Math.random() * width);
+            startY = Math.floor(Math.random() * height);
+
+        } while (startY == hatY && startX == hatX);
+        
+        array[startY][startX] = '*';
+        console.log(`start X = ${startX} start Y = ${startY}`);
+        console.log(`hatY = ${hatY} hatX = ${hatX}`);
+
+        return [array, startX, startY];
     }
+
+    
 }
 
 let newGame = Field.generateField(4, 4, 0.2);
-//console.log(newGame);
+//newGame = Field.randomStart(newGame);
+//console.log(Field.startX);
 const myField = new Field(newGame);
+//console.log(newGame);
+
 myField.gameLogic();
